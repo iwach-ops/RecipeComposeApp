4d4620c89ff61e5c7e66909d909276ba1fed2719
I Wachtel <iwachtel73@gmail.com>
2026-04-08 13:43:14 +0200

    fix fallbackPainter remember

--- app/src/main/java/com/wachtel/androidrecipesapp/ui/recipes/RecipeItem.kt
+++ app/src/main/java/com/wachtel/androidrecipesapp/ui/recipes/RecipeItem.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.ColorPainter
@@ -27,7 +28,10 @@ fun RecipeItem(
     onClick: (Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val fallbackPainter = ColorPainter(MaterialTheme.colorScheme.surfaceVariant)
+    val surfaceVariant = MaterialTheme.colorScheme.surfaceVariant
+    val fallbackPainter = remember(surfaceVariant) {
+        ColorPainter(surfaceVariant)
+    }
 
     Card(
         modifier = modifier
