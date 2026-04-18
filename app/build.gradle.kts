55d15c904bee6f307fb5fa8fc33b0d4a6bb6fc73
I Wachtel <iwachtel73@gmail.com>
2026-04-18 12:50:37 +0200

    RCA-22-migrating-to-jetpack-datastore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     debugImplementation(libs.androidxComposeUiTestManifest)
     implementation(libs.coilCompose)
     implementation(libs.androidxNavigationCompose)
+    implementation(libs.androidxDatastorePreferences)
 }
