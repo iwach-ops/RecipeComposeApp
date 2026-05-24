fdcd322ebafd6a82a5bdbb6c82aa7ffc623cd24d
I Wachtel <iwachtel73@gmail.com>
2026-05-24 19:11:22 +0200

    RCA-40-data-access-object

--- app/src/main/java/com/wachtel/androidrecipesapp/data/database/dao/CategoryDao.kt
+++ app/src/main/java/com/wachtel/androidrecipesapp/data/database/dao/CategoryDao.kt
@@ -1,6 +1,8 @@
 package com.wachtel.androidrecipesapp.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.wachtel.androidrecipesapp.data.database.entity.CategoryEntity
 import kotlinx.coroutines.flow.Flow
@@ -10,4 +12,7 @@ interface CategoryDao {
 
     @Query("SELECT * FROM categories ORDER BY name ASC")
     fun getAllCategories(): Flow<List<CategoryEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCategories(categories: List<CategoryEntity>)
 }
--- app/src/main/java/com/wachtel/androidrecipesapp/data/database/dao/RecipeDao.kt
+++ app/src/main/java/com/wachtel/androidrecipesapp/data/database/dao/RecipeDao.kt
@@ -1,6 +1,8 @@
 package com.wachtel.androidrecipesapp.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.wachtel.androidrecipesapp.data.database.entity.RecipeEntity
 import kotlinx.coroutines.flow.Flow
@@ -10,4 +12,16 @@ interface RecipeDao {
 
     @Query("SELECT * FROM recipes")
     fun getAllRecipes(): Flow<List<RecipeEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipes(recipes: List<RecipeEntity>)
+
+    @Query("SELECT * FROM recipes WHERE categoryId = :categoryId")
+    fun getRecipesByCategoryId(categoryId: Int): Flow<List<RecipeEntity>>
+
+    @Query("SELECT * FROM recipes WHERE id = :id")
+    fun getRecipeById(id: Int): Flow<RecipeEntity?>
+
+    @Query("SELECT * FROM recipes WHERE id IN (:ids)")
+    fun getRecipesByIds(ids: List<Int>): Flow<List<RecipeEntity>>
 }
